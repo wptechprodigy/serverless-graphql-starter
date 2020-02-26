022659bd349cc6b0b5c0dbd5b8b5b951107b3ae2
wptechprodigy <waheed.freelancer16@gmail.com>
2020-02-26 09:21:45 +0100

    add server implementations

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const ApolloServer = require('apollo-server').ApolloServer;
+const ApolloServerLambda = require('apollo-server-lambda').ApolloServer;
+const { gql } = require('apollo-server-lambda');
+
+// Type and Resolver definitions
+const typeDefs = gql`
+  type Query {
+    introduce: String
+  }
+`;
+
+const resolver = {
+  Query: {
+    introduce: () => 'I am a serverless template from Waheed Afolabi 😎.',
+  },
+};
+
+// Create both factory functions (for serverless lambda and local)
+function createLamdbaServer() {
+  return new ApolloServer({
+    typeDefs,
+    resolvers,
+    introspection: true,
+    playground: true,
+  });
+}
+
+function createLocalServer() {
+  return new ApolloServer({
+    typeDefs,
+    resolvers,
+    introspection: true,
+    playground: true,
+  });
+}
